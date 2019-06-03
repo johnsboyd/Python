4f7bdf330b847b4f1f0977e4a927570e355badcf
John Boyd <johnsboyd@gmail.com>
2019-06-03 14:46:01 -0700

    enable error handling

--- wordsoup/app.py
+++ wordsoup/app.py
@@ -54,15 +54,15 @@ def markov():
     lyrics = walk(all_dict)
     return render_template('markov.html', lyrics = lyrics) 
 
-#@app.errorhandler(500)
-#def internal_server_error(error):
-#    app.logger.error('Server Error: %s', (error))
-#    return render_template('500.html'), 500
-#
-#@app.errorhandler(Exception)
-#def unhandled_exception(e):
-#    app.logger.error('Unhandled Exception: %s', (e))
-#    return render_template('500.html'), 500
+@app.errorhandler(500)
+def internal_server_error(error):
+    app.logger.error('Server Error: %s', (error))
+    return render_template('500.html'), 500
+
+@app.errorhandler(Exception)
+def unhandled_exception(e):
+    app.logger.error('Unhandled Exception: %s', (e))
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0',debug=True)
