916a37db3c44e63d3dcd941b20031422be48ce4a
John Boyd <quispie12@yahoo.com>
2020-07-17 01:11:33 -0700

    new waveshare lcd hat

--- pdmenu/menu.py
+++ pdmenu/menu.py
@@ -1,205 +1,208 @@
-#!/usr/bin/python
-
-import time
+import curses
 import os
-
 import RPi.GPIO as GPIO
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_SSD1306
-
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-
 import subprocess
+import time
 
-########### SETUP ############
-# /dev/snd/midiC1D0 /dev/midi1  
-# /dev/snd/midiC2D0 /dev/midi2
+# GPIO define
+#RST_PIN        = 25
+#CS_PIN         = 8
+#DC_PIN         = 24
+#RST = 27
+#DC = 25
+#BL = 24
+#bus = 0 
+#device = 0 
+
+buttons = {'KEY_UP_PIN': 6, 'KEY_DOWN_PIN': 19, 'KEY_LEFT_PIN': 5,
+           'KEY_RIGHT_PIN': 26, 'KEY_PRESS_PIN': 13, 'KEY1_PIN': 21,
+           'KEY2_PIN': 20, 'KEY3_PIN': 16 }
+
+#init GPIO
+# for P4:
+# sudo vi /boot/config.txt
+# gpio=6,19,5,26,13,21,20,16=pu
+GPIO.setmode(GPIO.BCM) 
+for key in buttons:
+    GPIO.setup(buttons[key], GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
+
+
 if not os.path.islink('/dev/midi1'):
     os.symlink('/dev/snd/midiC1D0','/dev/midi1')
 if not os.path.islink('/dev/midi2'):
     os.symlink('/dev/snd/midiC2D0','/dev/midi2')
 
-# Raspberry Pi pin configuration:
-RST = None     # on the PiOLED this pin isnt used
-# 128x64 display with hardware I2C:
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
-# Initialize library.
-disp.begin()
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = disp.width 
-height = disp.height 
-image = Image.new('1', (width, height))
 
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
-# Load default font.
-#font = ImageFont.load_default()
-font = ImageFont.truetype('FreeSansBold.ttf', 9)
-
-# Input pins:
-L_pin = 27 
-R_pin = 23 
-C_pin = 4 
-U_pin = 17 
-D_pin = 22 
-A_pin = 5 
-B_pin = 6 
-
-preset = 0
-page = "main" 
-pdfiles = sorted([i for i in os.listdir('./') if i.endswith('.pd')])
-numpre = len(pdfiles)
-pdproc = ''
-
-GPIO.setmode(GPIO.BCM) 
-GPIO.setup(A_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
-GPIO.setup(B_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
-GPIO.setup(L_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
-GPIO.setup(R_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
-GPIO.setup(U_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
-GPIO.setup(D_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
-GPIO.setup(C_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
-
-# Clear display.
-disp.clear()
-disp.display()
-
-# Draw a black filled box to clear the image.
-#draw.rectangle((0,0,width,height), outline=0, fill=0)
-#disp.image(image)
-#disp.display()
-
-cmd = "hostname -I | awk '{print \"IP: \" $1}'"
-ip = subprocess.check_output(cmd, shell = True )
-
-
-########### FUNCTIONS ############
-
-def view(pre):
-    global preset
-    preset = pre
-    draw.rectangle((0,0,width-1,height-1), outline=0, fill=0)
-    font = ImageFont.truetype('FreeSansBold.ttf', 9)
-    draw.text((104, 0), "[info]", font=font, fill=255)
-    row=0
-    for x in range(pre-2,pre+3):
-        if x == pre:
-            font = ImageFont.truetype('FreeSansBold.ttf', 16)
-            draw.text((0, row-4), pdfiles[x % numpre][:-3],  font=font, fill=255)
+###### Begin state machine code ######
+class ScreenState(object):
+    
+    name = "state"
+    allowed = []
+    
+    def switch(self, state):
+        if state.name in self.allowed:
+            #print('Current:',self,' => switching to new state [',state.name,']')
+            self.__class__ = state
+            return True
         else:
-            font = ImageFont.truetype('FreeSansBold.ttf', 9)
-            draw.text((0, row), pdfiles[x % numpre][:-3],  font=font, fill=255)
-        row += 13
-    draw.text((105, 54), "[halt]", font=font, fill=255)
-    disp.image(image)
-    disp.display()
+            #print('Current:',self,' => switching to new state [',state.name,'] not possible.')
+            return False
 
-def up(channel):
-    view((preset - 1) % numpre)
-
-def down(channel):
-    view((preset + 1) % numpre)
-
-def pick(channel):
-    global pdproc
-    if pdproc != '':
-        pdproc.terminate()
-        pdproc.kill()
-    pdproc = subprocess.Popen(['puredata', '-nogui', '-midiindev', '1,2', '-midioutdev', '1,2', '-open', pdfiles[preset] ], stdout=subprocess.PIPE, shell=False)
-    # puredata -nogui -midiindev 1,2 -midioutdev 1,2 -open 
-    draw.rectangle((0,0,width-1,height-1), outline=1, fill=0)
-    font = ImageFont.truetype('FreeSansBold.ttf', 16)
-    draw.text((24, 12), "LOADING",  font=font, fill=255)
-    draw.text((2, 32), pdfiles[preset][:-3] ,  font=font, fill=255)
-    disp.image(image)
-    disp.display()
-    view(preset)
+    def __str__(self):
+        return self.name
     
+class Menu(ScreenState):
+    name = "menu"
+    allowed = ['up','down','load','halt']
 
-def info(channel):
-    global page
-    if page == "fin":
-        page = "main"
-        view(preset)
-    elif page == "main":
-        cmd = "w | head -1 | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-        load = subprocess.check_output(cmd, shell = True )
-        cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%smb %.2f%%\", $3,$2,$3*100/$2 }'"
-        mem = subprocess.check_output(cmd, shell = True )
-        draw.rectangle((0,0,width-1,height-1), outline=0, fill=0)
-        page = "info"
-        font = ImageFont.truetype('FreeSansBold.ttf', 9)
-        draw.text((0, 0), ip, font=font, fill=255)
-        draw.text((0, 9), load, font=font, fill=255)
-        draw.text((0, 18), mem, font=font, fill=255)
-        if pdproc != '':
-            draw.text((0,27), "Name: "+pdfiles[preset][:-3], font=font, fill=255)
-        draw.text((102, 0), "[back]", font=font, fill=255)
-        disp.image(image)
-        disp.display()
-    elif page == "info":
-        page = "main"
-        view(preset)
-        
+class Up(ScreenState):
+    name = "up"
+    allowed = ['up','down','load','halt','menu']
 
-def kill(channel):
-    global pdproc
-    if pdproc != '':
-        pdproc.terminate()
-        pdproc.kill()
+class Down(ScreenState):
+    name = "down"
+    allowed = ['up','down','load','halt','menu']
+
+class Load(ScreenState):
+    name = "load"
+    allowed = ['menu','info','clear']
+
+class Clear(ScreenState):
+    name = "clear"
+    allowed = ['menu','clear','info']
+
+class Info(ScreenState):
+    name = "info"
+    allowed = ['load']
+
+class Halt(ScreenState):
+    name = "halt"
+    allowed = ['menu','off']
+
+class Off(ScreenState):
+    name = "off"
+    allowed = []
+
+class On(ScreenState):
+    name = "on"
+    allowed = ['menu']
+
+class Screen(object):
+    def __init__(self):
+        # State of the screen - default is menu.
+        self.state = On()
+
+    def change(self, state):
+        """ Change state """
+        status = self.state.switch(state)
+        return status
+
+    def current(self):
+        return self.state
+
+###### End state machine code ######
+
+def rot(presets,num):
+    if num > 0:
+        for i in range(0,num):
+            p = presets.pop(0)
+            presets.append(p)
+    elif num < 0:
+        for i in (range(0,num,-1)):
+            p = presets.pop()
+            presets.insert(0,p)
+
+def menu(stdscr,scrn,presets):
+    if scrn.change(Menu):
+        stdscr.clear()
+        h, w = stdscr.getmaxyx()
+        x = 0
+        y = 0
+        for idx in range(0,12):
+            if idx == 6:
+                stdscr.addstr(y, x, presets[idx][:-3],  curses.A_UNDERLINE | curses.A_BOLD )
+            else:
+                #stdscr.attron(curses.color_pair(2))
+                stdscr.addstr(y, x, presets[idx][:-3], curses.color_pair(1) )
+            y += 1
+
+        stdscr.addstr(1,w-4, "LOAD", curses.color_pair(2))
+        stdscr.addstr(h-2,w-4, "HALT", curses.color_pair(2))
+        stdscr.refresh()
+
+def up(stdscr,scrn,presets):
+    if scrn.change(Up):
+        rot(presets,-1)
+        menu(stdscr,scrn,presets)
+
+def down(stdscr,scrn,presets):
+    if scrn.change(Down):
+        rot(presets,1)
+        menu(stdscr,scrn,presets)
+
+def key1(stdscr,scrn,presets):
+    prev = str(scrn.current())
+    if prev == 'load' or prev == 'clear' or prev == 'halt':
+        menu(stdscr,scrn, presets)
+    elif prev == 'menu' or prev == 'up' or prev =='down':
+        if scrn.change(Load):
+            prev = str(scrn.current())
+            stdscr.clear()
+            h, w = stdscr.getmaxyx()
+            stdscr.addstr(0,1, presets[6][:-3], curses.A_UNDERLINE | curses.A_BOLD )
+            stdscr.addstr(1,w-4, "MENU", curses.color_pair(2))
+            stdscr.addstr(h-2,w-4, "INFO", curses.color_pair(2))
+            stdscr.refresh()
+            subprocess.run(['pkill', '-u', 'root', 'puredata'])
+            pdproc = subprocess.Popen(['puredata', '-nogui', '-midiindev', '1', '-midioutdev', '1', '-open', presets[6] ], stdout=subprocess.PIPE, shell=False)
+            #pdproc = subprocess.Popen(['puredata', '-nogui', '-midiindev', '1,2', '-midioutdev', '1,2', '-open', presets[6] ], stdout=subprocess.PIPE, shell=False)
+            # puredata -nogui -midiindev 1,2 -midioutdev 1,2 -open 
     
+def key3(stdscr,scrn,presets):
+    prev = str(scrn.current())
+    if prev == 'halt':
+        if scrn.change(Off):
+            stdscr.clear()
+            h, w = stdscr.getmaxyx()
+            stdscr.addstr(h//2, 1, "Let green LED turn off", curses.A_UNDERLINE | curses.A_BOLD )
+            stdscr.refresh()
+            time.sleep( 2 )
+            subprocess.run(['pkill', '-u', 'root', 'puredata'])
+            cmd = "halt"
+            out = subprocess.check_output(cmd, shell = True )
+    elif prev == 'menu' or prev == 'up' or prev =='down':
+        if scrn.change(Halt):
+            stdscr.clear()
+            h, w = stdscr.getmaxyx()
+            cmd = "hostname -I | cut -d' ' -f 1"
+            ip = subprocess.check_output(cmd, shell = True ).decode("utf-8")
+            stdscr.addstr(1,w-4, "MENU", curses.color_pair(2))
+            stdscr.addstr(h//2-1, 1, "Shutdown. Are you sure?", curses.A_UNDERLINE | curses.A_BOLD )
+            stdscr.addstr(h-4,1, "IP: " + ip, curses.color_pair(1))
+            stdscr.addstr(h-2,w-3, "YES", curses.color_pair(2))
+            stdscr.refresh()
 
-def fin(channel):
-    global page
-    draw.rectangle((0,0,width-1,height-1), outline=0, fill=0)
-    if page == "fin":
-        draw.text((0, 32), "wait for green light to go off", font=font, fill=255)
-        disp.image(image)
-        disp.display()
-    	if pdproc != '':
-            pdproc.terminate()
-        cmd = "sudo halt"
-        out = subprocess.check_output(cmd, shell = True ) 
-    elif page == "main":
-        draw.text((102, 0), "[back]", font=font, fill=255)
-        draw.text((32, 32), "Are you sure?", font=font, fill=255)
-        draw.text((105, 54), "[halt]", font=font, fill=255)
-        page = "fin" 
-        disp.image(image)
-        disp.display()
-    elif page == "info":
-        page = "main"
-        info(B_pin)
-    
+def main(stdscr):
+    presets = sorted([i for i in os.listdir('./') if i.endswith('.pd')])
+    curses.curs_set(0)
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    rot(presets,-6) 
+    scrn = Screen()
+    menu(stdscr,scrn,presets)
 
-def main():
-    global pdproc
-    view(preset)
-    GPIO.add_event_detect(U_pin, GPIO.FALLING, callback=up, bouncetime=500)
-    GPIO.add_event_detect(D_pin, GPIO.FALLING, callback=down, bouncetime=500)
-    GPIO.add_event_detect(C_pin, GPIO.FALLING, callback=pick, bouncetime=700)
-    GPIO.add_event_detect(A_pin, GPIO.FALLING, callback=fin, bouncetime=500)
-    GPIO.add_event_detect(B_pin, GPIO.FALLING, callback=info, bouncetime=500)
-    GPIO.add_event_detect(R_pin, GPIO.FALLING, callback=kill, bouncetime=500)
+    # button interrupts section
+    GPIO.add_event_detect(buttons['KEY_UP_PIN'], GPIO.FALLING,callback=lambda x: up(stdscr,scrn,presets), bouncetime=200)
+    GPIO.add_event_detect(buttons['KEY_DOWN_PIN'], GPIO.FALLING,callback=lambda x: down(stdscr,scrn,presets), bouncetime=200)
+    GPIO.add_event_detect(buttons['KEY1_PIN'], GPIO.FALLING,callback=lambda x: key1(stdscr,scrn,presets), bouncetime=200)
+    GPIO.add_event_detect(buttons['KEY3_PIN'], GPIO.FALLING,callback=lambda x: key3(stdscr,scrn,presets), bouncetime=200)
+
     try:  
-        #GPIO.wait_for_edge(L_pin, GPIO.RISING)  
-        GPIO.wait_for_edge(L_pin, GPIO.FALLING)  
+        GPIO.wait_for_edge(buttons['KEY_LEFT_PIN'], GPIO.FALLING)  
   
     except KeyboardInterrupt:  
         GPIO.cleanup()       # clean up GPIO on CTRL+C exit  
     GPIO.cleanup()       # clean up GPIO on normal exit  
-    if pdproc != '':
-        pdproc.terminate()
-        pdproc.kill()
-    draw.rectangle((0,0,width-1,height-1), outline=0, fill=0)
-    disp.image(image)
-    disp.display()
+    subprocess.run(['pkill', '-u', 'root', 'puredata'])
 
-
-# Standard boilerplate to call the main() function to begin
-# the program.
-if __name__ == '__main__':
-    main() 
+curses.wrapper(main)
